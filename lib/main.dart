d648559a1dad8d33fa87c0dcdd6eb130d0108f32
minhvh5504 <vihongminh5504@gmail.com>
2024-12-07 01:18:12 +0700

    init: new project

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp() as Widget);
+}
+
+class MyApp {
+  const MyApp();
+}
