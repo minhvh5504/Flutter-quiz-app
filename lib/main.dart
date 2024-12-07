c4ae4851968c7b633c80466d78dbd17cc67c023e
minhvh5504 <vihongminh5504@gmail.com>
2024-12-07 18:25:18 +0700

    build base app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:app_flutter_demo/ui/screens/my_app.dart';
 
 void main(List<String> args) {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp() as Widget);
-}
-
-class MyApp {
-  const MyApp();
+  runApp(const MyApp());
 }
